e08657dbe41bce83af5521656318d13d62bcd654
juzi <zzh@youxiake.com>
2024-12-08 18:57:39 +0800

    Initial Commit

---
+++ picture2words/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  picture2words
+//
+//  Created by 庞梦婷 on 2024/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ picture2words/picture2wordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  picture2wordsApp.swift
+//  picture2words
+//
+//  Created by 庞梦婷 on 2024/12/8.
+//
+
+import SwiftUI
+
+@main
+struct picture2wordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
